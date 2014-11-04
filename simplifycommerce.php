d0d8ec52019015a40092e370d19454e15e0da395
Thomas Nabord <thomas.nabord@prestashop.com>
2014-11-04 17:01:58 +0100

    Fix conf name length for PS 1.5

--- simplifycommerce.php
+++ simplifycommerce.php
@@ -50,7 +50,7 @@ class SimplifyCommerce extends PaymentModule
 	{
 		$this->name = 'simplifycommerce';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.0.7';
+		$this->version = '1.0.8';
 		$this->author = 'MasterCard';
 		$this->need_instance = 0;
 
@@ -430,7 +430,7 @@ class SimplifyCommerce extends PaymentModule
 		}
 
 		if (Configuration::get('SIMPLIFY_MODE'))
-			Configuration::updateValue('SIMPLIFYCOMMERCE_CONFIGURATION_OK', true);
+			Configuration::updateValue('SIMPLIFYCOMMERCE_CONFIGURED', true);
 
 		if (version_compare(_PS_VERSION_, '1.5', '<'))
 			Tools::redirect(Link::getPageLink('order-confirmation.php', null, null).
