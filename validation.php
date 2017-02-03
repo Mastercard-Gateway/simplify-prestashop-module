4cd200d002ac1d5a76e3e56707d3e9573a510928
Ted Sanders <ted_sanders@mastercard.com>
2017-02-03 15:05:14 -0600

    tabs to spaces

--- validation.php
+++ validation.php
@@ -30,8 +30,9 @@ include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../init.php');
 include(dirname(__FILE__).'/simplifycommerce.php');
 
-if (!defined('_PS_VERSION_'))
-	exit;
+if (!defined('_PS_VERSION_')){
+    exit;
+}
 
 $simplify = new SimplifyCommerce();
 $simplify->processPayment();
