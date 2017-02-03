3f3843cc5823b05b34cc256094031292ec48cd95
Ted Sanders <ted_sanders@mastercard.com>
2017-02-03 16:05:56 -0600

    update php sdk user agent

--- lib/Simplify.php
+++ lib/Simplify.php
@@ -57,7 +57,7 @@ class Simplify
     /**
     * @var string $user_agent User-agent string send with requests.
     */
-    public static $user_agent = 'PrestaShop';
+    public static $user_agent = 'PrestaShop-1.2.0';
 }
 
 require_once(dirname(__FILE__).'/Simplify/Object.php');
