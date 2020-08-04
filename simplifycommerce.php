98f1554c03c78c9befe6e08ca0d6c9377cbf9345
Kristjan Heinaste <kristjan@ontapgroup.com>
2020-08-04 16:12:55 +0300

    Process Hosted payment fields through translit before passing them to gateway

--- simplifycommerce.php
+++ simplifycommerce.php
@@ -363,17 +363,17 @@ class SimplifyCommerce extends PaymentModule
         // Set js variables to send in card tokenization
         $this->smarty->assign('simplify_public_key', Simplify::$publicKey);
 
-        $this->smarty->assign('firstname', $cardholder_details->firstname);
-        $this->smarty->assign('lastname', $cardholder_details->lastname);
-        $this->smarty->assign('city', $cardholder_details->city);
-        $this->smarty->assign('address1', $cardholder_details->address1);
-        $this->smarty->assign('address2', $cardholder_details->address2);
-        $this->smarty->assign('state', isset($cardholder_details->state)?$cardholder_details->state:'');
-        $this->smarty->assign('postcode', $cardholder_details->postcode);
+        $this->smarty->assign('firstname', $this->safe($cardholder_details->firstname));
+        $this->smarty->assign('lastname', $this->safe($cardholder_details->lastname));
+        $this->smarty->assign('city', $this->safe($cardholder_details->city));
+        $this->smarty->assign('address1', $this->safe($cardholder_details->address1));
+        $this->smarty->assign('address2', $this->safe($cardholder_details->address2));
+        $this->smarty->assign('state', isset($cardholder_details->state)?$this->safe($cardholder_details->state):'');
+        $this->smarty->assign('postcode', $this->safe($cardholder_details->postcode));
 
         //fields related to hosted payments
-        $this->smarty->assign('hosted_payment_name', $this->context->shop->name);
-        $this->smarty->assign('hosted_payment_description', $this->context->shop->name.$this->l(' Order Number: ').(int)$this->context->cart->id);
+        $this->smarty->assign('hosted_payment_name', $this->safe($this->context->shop->name));
+        $this->smarty->assign('hosted_payment_description', $this->safe($this->context->shop->name).$this->l(' Order Number: ').(int)$this->context->cart->id);
         $this->smarty->assign('hosted_payment_reference', 'Order Number'.(int)$this->context->cart->id);
         $this->smarty->assign('hosted_payment_amount', ($this->context->cart->getOrderTotal() * 100));
 
@@ -386,6 +386,30 @@ class SimplifyCommerce extends PaymentModule
         $option = $this->getPaymentOption();
 
         return [$option];
+    }
+
+    protected function safe($field)
+    {
+        $copy = $field;
+        $encoding = mb_detect_encoding($field);
+        if ($encoding !== 'ASCII') {
+            if (function_exists('transliterator_transliterate')) {
+                $field = transliterator_transliterate('Any-Latin; Latin-ASCII', $field);
+            } else if (function_exists('iconv')) {
+                // fall back to iconv if intl module not available
+                $field = iconv($encoding, 'ASCII//TRANSLIT//IGNORE', $field);
+                $field = str_ireplace('?', '', $field);
+                $field = trim($field);
+            } else {
+                // no transliteration possible, revert to original field
+                return $field;
+            }
+            if (!$field) {
+                // if translit turned the string into any false-like value, return original instead
+                return $copy;
+            }
+        }
+        return $field;
     }
 
     public function getPaymentOption()
